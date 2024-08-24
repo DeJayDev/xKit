b3586ee4417c4df774c32d6ed1aeb27ddaa2b008
Dj <contact@dejaydev.com>
2024-08-24 01:18:05 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# xKit
+
+Someone asked for a plugin that gave players a kit on respawn, so I made it.
+
+### License
+
+This project uses the MIT License, which allows you to freely use, copy, modify, and distribute the software for any purpose, with the condition that you include the original copyright notice and the MIT License text in your copies.
+
+However, there are limitations:
+* You cannot hold the copyright holders liable for any issues.
+* You cannot remove or modify the MIT License text.
+* You cannot use the names of the copyright holders for endorsements without permission.
---
+++ src/main/java/dev/dejay/xKit/XKit.java
@@ -0,0 +1,74 @@
+package dev.dejay.xKit;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class XKit extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    @EventHandler
+    public void onDeath(PlayerDeathEvent e) {
+        e.getDrops().clear(); // force the player not to drop anything
+    }
+
+    @EventHandler
+    public void onRespawn(PlayerRespawnEvent event) {
+        Player player = event.getPlayer();
+        PlayerInventory inventory = player.getInventory();
+
+        // Define the items:
+        ItemStack helmet = new ItemStack(Material.LEATHER_HELMET, 1);
+        // Example for defining custom metadata, like a name, setting an enchant and removing the enchant list thing
+        ItemMeta helmetMeta = helmet.getItemMeta();
+        helmetMeta.displayName(Component.text("woah!", NamedTextColor.RED));
+        helmetMeta.addEnchant(Enchantment.SHARPNESS, 1, false);
+        helmetMeta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+        helmet.setItemMeta(helmetMeta);
+
+        ItemStack chestplate = new ItemStack(Material.LEATHER_CHESTPLATE, 1);
+        ItemStack leggings = new ItemStack(Material.LEATHER_LEGGINGS, 1);
+        ItemStack boots = new ItemStack(Material.LEATHER_BOOTS, 1);
+
+        ItemStack sword = new ItemStack(Material.STONE_SWORD, 1);
+        ItemStack shield = new ItemStack(Material.SHIELD, 1);
+
+        ItemStack food = new ItemStack(Material.COOKED_BEEF, 16);
+        ItemStack wool = new ItemStack(Material.PINK_WOOL, 32);
+
+        // using equipment slots to equip items,
+        inventory.setItem(EquipmentSlot.HEAD, helmet);
+        inventory.setItem(EquipmentSlot.CHEST, chestplate);
+        inventory.setItem(EquipmentSlot.LEGS, leggings);
+        inventory.setItem(EquipmentSlot.FEET, boots);
+
+        inventory.setItem(EquipmentSlot.HAND, sword);
+        inventory.setItem(EquipmentSlot.OFF_HAND, shield);
+
+        inventory.addItem(food, wool); // notice you can just put however many you want in these
+
+    }
+
+}
